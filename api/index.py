39c2a062da4dec3fea64485174a2f8d0a6fd8313
Dang <wheodnjs2@naver.com>
2026-06-09 19:41:24 +0900

    index.py 수정

--- api/index.py
+++ api/index.py
@@ -27,5 +27,6 @@ def search():
     return render_template("search.html", keyword=keyword, jobs=db[keyword])
 
 # app.run("0.0.0.0") 
-# if __name__ == "__main__":
-    # app.run("0.0.0.0")
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
